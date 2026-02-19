c1eb994c71548b4480501f67139e54fc805b445e
Mocas-12 <a18577y@gmail.com>
2026-02-19 20:57:31 +0900

    vercel-final-test-backend-alive

--- api/identify.js
+++ api/identify.js
@@ -1,9 +1,11 @@
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  if (req.method === 'GET' || req.method === 'OPTIONS') {
-    return res.status(200).send('Backend is Alive')
+module.exports = async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Content-Type', 'text/plain')
+    return res.status(200).send('Final Test: Backend is really alive')
+  } catch (e) {
+    return res.status(500).send('Crash: ' + (e && e.message ? e.message : String(e)))
   }
-  return res.status(200).json({ message: 'Ready for POST' })
 }
