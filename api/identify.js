1b18be7b01110173f22c3b2aa3607f3ecbfe5429
Mocas-12 <a18577y@gmail.com>
2026-02-19 21:21:08 +0900

    vercel-final-json-only

--- api/identify.js
+++ api/identify.js
@@ -1,14 +1,7 @@
 module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  try {
-    const info = {
-      method: req.method,
-      hasKey: !!process.env.NVIDIA_API_KEY,
-      message: 'Backend Is Active',
-    }
-    return res.status(200).send(JSON.stringify(info, null, 2))
-  } catch (err) {
-    return res.status(500).send('Server Error: ' + (err && err.message ? err.message : String(err)))
-  }
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.status(200).json({
+    message: 'Final Test Success',
+    key: !!process.env.NVIDIA_API_KEY,
+  })
 }
