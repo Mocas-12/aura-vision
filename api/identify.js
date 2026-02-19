a9de4162b9567e127584eb48d8134f4eb171a101
Mocas-12 <a18577y@gmail.com>
2026-02-19 21:01:56 +0900

    vercel-robust-identify-json-info

--- api/identify.js
+++ api/identify.js
@@ -1,11 +1,16 @@
 module.exports = async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   try {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    res.setHeader('Content-Type', 'text/plain')
-    return res.status(200).send('Final Test: Backend is really alive')
-  } catch (e) {
-    return res.status(500).send('Crash: ' + (e && e.message ? e.message : String(e)))
+    const info = {
+      method: req.method,
+      hasKey: !!process.env.NVIDIA_API_KEY,
+      message: 'Backend Is Active',
+    }
+    return res.status(200).send(JSON.stringify(info, null, 2))
+  } catch (err) {
+    return res
+      .status(500)
+      .send('Server Error: ' + (err && err.message ? err.message : String(err)))
   }
 }
